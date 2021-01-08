34b97a2cb12c2b50f0964e2505db05507d521644
Keith Weiss <fweissi@me.com>
2021-01-08 14:23:35 -0700

    Update Package.swift
    Added Forked Dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "LiquidAwsS3Driver", targets: ["LiquidAwsS3Driver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/binarybirds/liquid-kit.git", from: "1.2.0-beta"),
+        .package(url: "https://github.com/fweissi/liquid-kit.git", from: "1.2.0-beta"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.34.0"),
         .package(url: "https://github.com/soto-project/soto.git", from: "5.0.0")
     ],
